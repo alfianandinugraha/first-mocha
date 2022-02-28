fdc7874be41c645c2132277599806a345983b72a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-03-01 01:21:42 +0700

    feat(.mocharc.cjs): generate reports

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -2,7 +2,7 @@ const Mocha = require("mocha");
 
 const mocha = new Mocha({
   reporter: "mochawesome",
-  reporterOptions: ["reportDir=reports"],
+  reporterOptions: ["reportDir=reports", "reportFilename=index"],
 });
 
 module.exports = mocha.options;
